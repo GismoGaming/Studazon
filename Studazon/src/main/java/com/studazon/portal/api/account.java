1631ba5597c5a281c111531830a0d03e892a5cb0
ukpatell <umangpatelr4@gmail.com>
2023-04-04 00:00:27 -0400

    Account update feature added

--- Studazon/src/main/java/com/studazon/portal/api/account.java
+++ Studazon/src/main/java/com/studazon/portal/api/account.java
@@ -1,22 +1,69 @@
 package com.studazon.portal.api;
 
+import com.studazon.portal.dao.UserDAO;
+import com.studazon.portal.entity.User;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.codec.digest.Crypt;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet(name = "account", value = "/account")
 public class account extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setAttribute("user", request.getSession().getAttribute("user"));
         request.getRequestDispatcher("account.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action.equals("update_profile")) {
+            try {
+                update_profile(request, response);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void update_profile(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+        String fullname = request.getParameter("fullname");
+        String old_pswd = request.getParameter("old_password");
+        String new_pswd = request.getParameter("new_password");
+
+        if (fullname == null || fullname.isEmpty()) {
+            request.setAttribute("status", "failed");
+            request.setAttribute("message", "Invalid request");
+            request.getRequestDispatcher("account.jsp").include(request, response);
+            return;
+        }
+
+        User userRecord = (User) request.getSession().getAttribute("user");
+        userRecord.setFullname(fullname);
+
+        if (old_pswd != null && !old_pswd.isEmpty() && new_pswd != null && !new_pswd.isEmpty()) {
+            if (!Crypt.crypt(old_pswd, "$1$SZ").equals(userRecord.getPassword())) {
+                request.setAttribute("status", "failed");
+                request.setAttribute("message", "Incorrect Password");
+                request.getRequestDispatcher("account.jsp").include(request, response);
+                return;
+            }
+            userRecord.setPassword(new_pswd);
+        }
+
+        UserDAO.updateUser(userRecord);
+        request.setAttribute("user", userRecord);
+
+        request.setAttribute("status", "success");
+        request.setAttribute("message", "Profile Updated!");
+        request.getRequestDispatcher("account.jsp").include(request, response);
 
     }
 }
--- Studazon/src/main/webapp/account.jsp
+++ Studazon/src/main/webapp/account.jsp
@@ -13,6 +13,11 @@
     <link rel="stylesheet" href="assets/css/account.css">
     <title>Studazon - My Account</title>
 </head>
+
+<%-- Alert Code (has to be on every page)--%>
+<input type="hidden" id="status" value="<%= request.getAttribute("status")%>">
+<input type="hidden" id="message" value="<%= request.getAttribute("message")%>">
+
 <body>
 <div class="background_circle">
     <form action="account">
@@ -28,18 +33,33 @@
 
 </div>
 
-<%--<div class="search-container">--%>
-<%--    <form action="listing">--%>
-<%--        <input type="image" src="./assets/Interactivity/Search%20Icon.png" alt="Search Icon">--%>
-<%--        <input class="input-field" type="text" placeholder="Search for a book...">--%>
-<%--    </form>--%>
-<%--</div>--%>
-
 <form action="listing">
     <input type="image" class="btn_createListing" src="./assets/Interactivity/Create New.png"
            alt="Create new listing icon">
 </form>
 
+<div class="content">
+    <h1>Account Profile</h1>
+    <form method="post" action="account">
+        <input type="hidden" name="action" value="update_profile"/>
+        <label for="fullname">Full Name:</label>
+        <input type="text" id="fullname" name="fullname" value="${user.fullname}" required>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" value="${user.email}" readonly>
+
+        <label for="old_password">Old Password:</label>
+        <input type="old_password" id="old_password" name="old_password">
+
+        <label for="new_password">New Password:</label>
+        <input type="new_password" id="new_password" name="new_password">
+
+        <button type="submit">Update Profile</button>
+    </form>
+</div>
+
 <!-- <%@include file="footer.jsp" %> -->
 </body>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script type="text/javascript" src="./assets/js/alert.js"></script>
 </html>
