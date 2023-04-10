3d77a338bf375b5695f695620757f1e5b6b041ea
ukpatell <umangpatelr4@gmail.com>
2023-04-09 23:54:46 -0400

    alerts fixed

--- Studazon/src/main/java/com/studazon/portal/api/register.java
+++ Studazon/src/main/java/com/studazon/portal/api/register.java
@@ -2,12 +2,14 @@ package com.studazon.portal.api;
 
 import com.studazon.portal.dao.UserDAO;
 import com.studazon.portal.entity.User;
+import com.studazon.portal.util.Mailer;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.mail.MessagingException;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
@@ -29,10 +31,15 @@ public class register extends HttpServlet {
 
         try {
             UserDAO.registerUser(user);
+            Mailer mailer = new Mailer();
+            mailer.setTo_name(user.getFullname());
+            mailer.setTo_email(user.getEmail());
+            mailer.setType_of_email("new_account");
+            mailer.send();
             response.sendRedirect("login.jsp");
         } catch (ClassNotFoundException | NoSuchAlgorithmException | SQLException ex) {
             throw new ServletException(ex);
-        } catch (NoSuchProviderException e) {
+        } catch (NoSuchProviderException | MessagingException e) {
             throw new RuntimeException(e);
         }
     }
